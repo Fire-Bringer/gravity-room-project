f863c30559670e89706fce149415b35386a65444
Rashad DuPaty <radupaty@gmail.com>
2025-01-09 16:33:12 +0900

    follow tutorial for second 2 sections of ScrollPanels.js

--- src/components/ScrollPanels.js
+++ src/components/ScrollPanels.js
@@ -13,7 +13,19 @@ function scrollPanels() {
           <span>Scroll</span>
         </p>
       </section>
-      <section></section>
+
+      <section className="section-col leading-none top-0 min-h-screen bg-[#0f0e0e] fixed left-0 right-0 bottom-0 z-[-1] w-full h-screen">
+        <div className="columns w-full relative flex justify-center gap-[2.5vw] h-full -rotate-45 items-center">
+          <div className="column-wrap relative flex flex-col pt-[5vh] px-0 pb-[15vh] z-1">
+            <div className="column relative block">
+              <div className="col-item w-[25vw] h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+                <img className="col-img" src="/images/img1.jpg">
+                </img>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       <section></section>
     </div>
   );
