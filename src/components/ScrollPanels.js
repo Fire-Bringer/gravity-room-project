9df3ef37d6ea4534b029d6b75122bbd9cef7691c
Rashad DuPaty <radupaty@gmail.com>
2025-01-09 16:21:06 +0900

    follow tutorial for first section of SrollPanels section

--- src/components/ScrollPanels.js
+++ src/components/ScrollPanels.js
@@ -3,11 +3,20 @@ import React from "react";
 function scrollPanels() {
   return (
     <div>
-      <section></section>
+      <section className="leading-none static top-0 w-full min-h-screen bg-[#0f0e0e] mb-[250vh] h-screen flex flex-col text-center items-center justify-center pt-32 pb-8">
+        <h2 className="section_title">
+          PANELS <br /> effect
+        </h2>
+
+        <p className="m-0 max-w-[400px]">BUILT USING NEXT.JS AND GSAP</p>
+        <p className="m-0 max-w-[400px] mt-auto text-red-500">
+          <span>Scroll</span>
+        </p>
+      </section>
       <section></section>
       <section></section>
     </div>
   );
-};
+}
 
 export default scrollPanels;
