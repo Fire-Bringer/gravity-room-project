92d3be26bc2e22cb9ab29b923a639d43c94dbd39
Rashad DuPaty <radupaty@gmail.com>
2025-02-24 12:05:15 +0900

    Testing yml edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import ScrollAnimation from "@/components/ScrollAnimation/ScrollAnimation";
 import Intro from '@/components/Intro/Intro';
 import About from "@/components/About/About";
-//import ScrollPanels from "@/components/ScrollPanels/ScrollPanels";
-//import Videos from "@/components/Videos/Videos";
-//import Photos from "@/components/Photos/Photos";
-//import Music from "@/components/Music/Music";
-//import Contact from "@/components/Contact/Contact";
+import ScrollPanels from "@/components/ScrollPanels/ScrollPanels";
+import Videos from "@/components/Videos/Videos";
+import Photos from "@/components/Photos/Photos";
+import Music from "@/components/Music/Music";
+import Contact from "@/components/Contact/Contact";
 
 const HomePage = () => {
 
@@ -17,6 +17,16 @@ const HomePage = () => {
         <Intro/>
 
         <About/>
+
+        <ScrollPanels/>
+
+        <Videos/>
+
+        <Photos/>
+
+        <Music/>
+
+        <Contact/>
 
       </ScrollAnimation>
 
