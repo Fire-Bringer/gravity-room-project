c89cda0af1ae2533438a1dc5c0fb917df5c4b512
Rashad DuPaty <radupaty@gmail.com>
2025-02-25 22:06:45 +0900

    Testing more efficient fetch method

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,34 +7,30 @@ import Photos from "@/components/Photos/Photos";
 import Music from "@/components/Music/Music";
 import Contact from "@/components/Contact/Contact";
 
-async function getAbout() {
+async function getData() {
   // Fetch data
   const resAbout = await fetch(process.env.NEXT_PUBLIC_BASE_URL + '/rcms-api/3/about/3');
   const about = await resAbout.json();
 
-  return about
-}
-
-async function getVideos() {
   const resVideos = await fetch(process.env.NEXT_PUBLIC_BASE_URL + '/rcms-api/4/videos/4');
   const videos = await resVideos.json();
 
-  return videos
-
-}
-
-async function getPhotos() {
   const resPhotos = await fetch(process.env.NEXT_PUBLIC_BASE_URL + '/rcms-api/5/photos/6');
   const photos = await resPhotos.json();
 
-  return photos
+  const data = [about, videos, photos]
+
+  return data
 }
+
 
 async function HomePage() {
 
-  const about = await getAbout()
-  const videos = await getVideos()
-  const photos = await getPhotos()
+  const data = await getData()
+
+  const about = data[0]
+  const videos = data[1]
+  const photos = data[2]
 
   return (
     <main>
