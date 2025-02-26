9d20aa784abb421aaa37692b3eccf5db136e6c5d
Rashad DuPaty <radupaty@gmail.com>
2025-02-26 17:49:42 +0900

    Testing ScrollPanels animation

--- src/components/ScrollPanels/ScrollPanels.jsx
+++ src/components/ScrollPanels/ScrollPanels.jsx
@@ -2,18 +2,27 @@
 import Lenis from "lenis";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function ScrollPanels() {
 
-  gsap.registerPlugin(ScrollTrigger);
+  const sectionRef = useRef(null);
+  const wrapRefs = useRef([]);
+  const itemRefs = useRef([]);
+
+  console.log("wrapRefs:", wrapRefs.current);
+  console.log("itemRefs:", itemRefs.current);
+
 
   useEffect(() => {
-    const dom = {
-      columns: document.querySelector(".section-col"),
-      columnWraps: document.querySelectorAll(".section-col .column-wrap"),
-      items: document.querySelectorAll(".section-cols .col-item"),
-    };
+
+    if (!sectionRef.current || wrapRefs.current.length === 0 || itemRefs.current.length === 0) {
+      console.warn("Elements not found.");
+      return;
+    }
+
 
     let lenis;
 
@@ -23,14 +32,16 @@ function ScrollPanels() {
         smooth: true,
       });
 
+      lenis.on("scroll", ScrollTrigger.update);
+
       const ScrollFn = (time) => {
         lenis.raf(time);
-
         requestAnimationFrame(ScrollFn);
       };
 
       requestAnimationFrame(ScrollFn);
     };
+
 
     const ScrollAni = () => {
       gsap
@@ -43,7 +54,7 @@ function ScrollPanels() {
         })
         .addLabel("start", 0)
         .to(
-          dom.columns,
+          sectionRef.current,
           {
             ease: "none",
             startAt: { scale: 1.2 },
@@ -51,7 +62,7 @@ function ScrollPanels() {
           },
           "start"
         )
-        .to(dom.items, {
+        .to(itemRefs.current, {
           scrollTrigger: {
             start: 0,
             end: "top top",
@@ -60,17 +71,15 @@ function ScrollPanels() {
 
           ease: "power4.inOut",
           startAt: {
-            opacity: 0,
-            filter: "brightness(300%)",
+            opacity: 1,
+            filter: "brightness(100%)",
+            yoyo: true,
+            repeat: 1,
           },
 
-          opacity: 1,
-          filter: "brightness(100%)",
-          yoyo: true,
-          repeat: 1,
         })
         .to(
-          dom.columnWraps,
+          wrapRefs.current,
           {
             ease: "none",
             yPercent: (pos) => pos * -15,
@@ -81,40 +90,40 @@ function ScrollPanels() {
 
     SmoothScroll();
     ScrollAni();
-  });
+  }, []);
 
   return (
     <div>
 
-      <section className="section-col leading-none top-0 min-h-screen bg-[#0f0e0e] fixed left-0 right-0 bottom-0 z-[-1] w-full h-screen">
+      <section className="section-col leading-none top-0 min-h-screen bg-[#0f0e0e] fixed left-0 right-0 bottom-0 z-[-1] w-full h-screen" ref={sectionRef}>
         <div className="columns w-full relative flex justify-center gap-[2.5vw] h-full -rotate-45 items-center">
 
           {/* Column 1*/}
 
-          <div className="column-wrap relative flex flex-col pt-[5vh] px-0 pb-[15vh] z-1">
+          <div className="column-wrap relative flex flex-col pt-[5vh] px-0 pb-[15vh] z-1" ref={(el) => wrapRefs.current.push(el)}>
             <div className="column relative block">
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
             </div>
@@ -122,30 +131,30 @@ function ScrollPanels() {
 
           {/* Column 2*/}
 
-          <div className="column-wrap relative flex flex-col pt-[5vh] px-0 pb-[15vh] z-1">
+          <div className="column-wrap relative flex flex-col pt-[5vh] px-0 pb-[15vh] z-1" ref={(el) => wrapRefs.current.push(el)}>
             <div className="column relative block">
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
             </div>
@@ -153,30 +162,30 @@ function ScrollPanels() {
 
           {/* Column 3*/}
 
-          <div className="column-wrap relative flex flex-col pt-[5vh] px-0 pb-[15vh] z-1">
+          <div className="column-wrap relative flex flex-col pt-[5vh] px-0 pb-[15vh] z-1" ref={(el) => wrapRefs.current.push(el)}>
             <div className="column relative block">
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
-              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0">
+              <div className="col-item w-[50vw] md:w-[30vw] lg:w-[25vw] h-[50vw] md:h-[30vw] lg:h-[25vw] relative overflow-hidden rounded-[4px] cursor-pointer mt-0 mr-0 mb-[2.5vw] ml-0" ref={(el) => itemRefs.current.push(el)}>
                 <img className="col-img" src="/images/space-hero.png" />
               </div>
             </div>
